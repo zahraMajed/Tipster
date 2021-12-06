fb3bfe0db4fdbc4280070303d3f6468167a04be4
Zahra Alzawad <zahra_m.alzawad@hotmail.com>
2021-12-06 12:57:32 +0000

    Initial Commit

---
+++ Tipster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipster
+//
+//  Created by admin on 06/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
